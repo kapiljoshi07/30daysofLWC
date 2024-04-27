56e4feb8ec7eb21a996c24838e1cbc7d5b459e09
kapiljoshi07 <kapil.18september@gmail.com>
2024-04-27 23:04:47 +0530

    learning composition

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -2,13 +2,28 @@ import { LightningElement, api } from 'lwc';
 
 export default class ChildComponent extends LightningElement {
 
+  /* Passing a primitive value */
   // @api itemName;
 
-  @api message;
-  @api className;
+  // @api message;
+  // @api className;
+
+  @api obj;
   
-  get getClassName(){
-    return this.className ? `alert ${this.className}`: `alert`;
+  // get getClassName(){
+  //   return this.className ? `alert ${this.className}`: `alert`;
+  // }
+
+  get serializedObj(){
+    return JSON.stringify(this.obj);
+  }
+
+  updateOriginal(){
+    this.obj.msg += "!!!";
+  }
+
+  updateShallow(){
+    this.obj = {...this.obj, msg: this.obj.msg + "!"};
   }
 
 }
--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,3 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class ParentComponent extends LightningElement {}
+export default class ParentComponent extends LightningElement {
+
+  @api obj = {
+    msg: "hello"
+  };
+
+  get serializedObj(){
+    return JSON.stringify(this.obj);
+  }
+}
